6faa09a1114739baf4944820f45be67f922f12df
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2020-10-08 13:58:33 +0200

    Functional tested tibble_to_raw_genotypes

--- R/tibble_to_raw_genotypes.R
+++ R/tibble_to_raw_genotypes.R
@@ -25,21 +25,21 @@
 #' @param progress report progress every N individuals
 #' @export
 tibble_to_raw_genotypes <- function(x, output = 'genotypes.raw', progress = 1) {
-  #   test_data <- tibble(
-  #     chr = c(1),
-  #     pos = c(1001,1002,1005,1017),
-  #     snp = c('snp1', 'snp2', 'snp3', 'snp4'),
-  #     strand = c('u'),
-  #     ind1 = c(0,1,1,2),
-  #     id2 = c(2,NA,1,1),
-  #     john = c(0,0,1,0),
-  #     jill = c(1,1,1,2),
-  #     elon = c(2,0,NA,1)
-  #   )
-  #
-  # x <- test_data
-  # output <- "genotypes.raw"
-  # progress <- 1
+    test_data <- tibble(
+      chr = c(1),
+      pos = c(1001,1002,1005,1017),
+      snp = c('snp1', 'snp2', 'snp3', 'snp4'),
+      strand = c('u'),
+      ind1 = c(0,1,1,2),
+      id2 = c(2,NA,1,1),
+      john = c(0,0,1,0),
+      jill = c(1,1,1,2),
+      elon = c(2,0,NA,1)
+    )
+
+  x <- test_data
+  output <- "genotypes.raw"
+  progress <- 1
 
   ids <- colnames(x)[-c(1:4)]
   n_ids <- length(ids)
@@ -51,7 +51,7 @@ tibble_to_raw_genotypes <- function(x, output = 'genotypes.raw', progress = 1) {
   n_markers <- length(marker_names)
   chrom <- x$chr
   pos <- x$pos
-  nbytes <- ceiling(num_ids/4)
+  nbytes <- ceiling(n_ids/4)
   coding <- as.raw(rep(1, length(pos)))
   strand <- as.raw(rep(0, length(pos)))
 
@@ -64,9 +64,9 @@ tibble_to_raw_genotypes <- function(x, output = 'genotypes.raw', progress = 1) {
   cat(file = ofile, coding, "\n")
   cat(file = ofile, strand, "\n")
   rdta <- raw(nbytes)
-  for (i in 1:n_ids) {
-    #i <- 1
-    gtin <- x %>% pull(ids[i])
+  i <- 1
+  for (marker in marker_names) {
+    gtin <- x %>% filter(snp == marker) %>% select(all_of(ids))
     gchk <- (gtin == 0 | gtin == 1 | gtin == 2 | gtin == 3)
     if (!all(gchk)) {
       cat("Wrong genotype codes:\nCODE\tID\tSNP\n")
@@ -79,6 +79,7 @@ tibble_to_raw_genotypes <- function(x, output = 'genotypes.raw', progress = 1) {
     cat(file = ofile, rdta, "\n")
     if (progress && round(i/progress) == i/progress)
       cat("Converted", i, "records...\n")
+  i <- i + 1
   }
   close(ofile)
 }
