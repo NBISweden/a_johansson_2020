5b3a6a2ed1932906eefd7623c93b2ae4f72ab6c7
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2020-10-19 15:09:15 +0200

    Added simulations for testing SKATO

--- R/GenABEL_common_test.R
+++ R/GenABEL_common_test.R
@@ -85,3 +85,29 @@ points(qt[simulated,]$Position, -log10(qt[simulated,]$P1df), col=col, pch=19)
 plot(rare$effects, qt[simulated, ]$effB, cex = .5, pch = 19, las=1, cex.axis=.7, xlab='simulated effect', ylab='predicted effect')
 grid(col='grey')
 abline(a = 0, b=1, col='lightgrey')
+
+# Now, let's look at the SKAT model
+library(SKAT)
+
+run_SKAT_test <- function(N = 10, maf, data) {
+  result <- rep(NA, times = N)
+    i <- 1
+    for (i in 1:N) {
+    rare <- get_effects(maf = maf, thr = 0.01,
+                      N = 20,
+                      shape12 = c(1,25),
+                      rare = T,
+                      frac_negative = .2)
+    genos_rare <- data@gtdata[,rare$marker_idx]
+    G_rare <- as.double(genos_rare) %>% fix_allele_encoding() %>% impute_G(maf = maf[rare$marker_idx])
+    y_rare <- G_rare %*% rare$effects + rnorm(n = dim(G_rare)[1], mean = 0, sd = 1)
+    srdta@phdata[,'qt1'] <- y_rare
+    skat_null_model <- SKAT_Null_Model(qt1~sex, data = srdta@phdata)
+    skat_model <- SKAT(G_rare, skat_null_model, method = 'SKATO')
+    result[i] <- skat_model$p.value
+    i <- i + 1
+  }
+  return(result)
+}
+
+run_SKAT_test(N = 100, maf = maf, data = srdta)
