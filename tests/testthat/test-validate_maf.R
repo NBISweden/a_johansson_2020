e6bf90212d4b7338cc9e33fc252f8e0440112ff6
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2020-11-18 16:39:36 +0100

    still fixing unit tests

--- tests/testthat/test-validate_maf.R
+++ tests/testthat/test-validate_maf.R
@@ -1,14 +1,14 @@
-maf <- c(0.1,0.2,0.3)
 test_that("validate maf works for well-behaved maf values", {
+  maf <<- c(0.1,0.2,0.3)
   tmp <- validate_maf(maf)
   expect_null(tmp[[1]])
   expect_null(tmp[[2]])
   expect_equal(tmp[[3]], maf)
 })
 
-maf <- c(0.1,0.2,0.7)
-tmp <- suppressWarnings(validate_maf(maf))
 test_that("validate maf works for ill-behaved maf values, greater than 0.5", {
+  maf <<- c(0.1,0.2,0.7)
+  tmp <- suppressWarnings(validate_maf(maf))
   expect_warning(validate_maf(maf))
   expect_false(is.null(tmp[[1]]))
   expect_equal(tmp[[2]], 0.7)
