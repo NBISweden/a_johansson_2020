e8700a70fa3b671cdb365a864afcd67490823116
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2020-11-19 16:40:28 +0100

    fixing tests

--- tests/testthat/test-get_maf.R
+++ tests/testthat/test-get_maf.R
@@ -4,4 +4,5 @@ test_that("getting maf works", {
   expect_warning(get_maf(srdta))
   maf <- suppressWarnings(get_maf(srdta))
   expect_true(near(maf[1], 0.1325503356))
+  expect_error(get_maf(srdta@gtdata))
 })
