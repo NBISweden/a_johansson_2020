c261433fdcdff4118809f78e62befe24ffd2e401
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2021-04-14 10:54:33 +0200

    Clean unused functions

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
-# global reference to scipy (will be initialized in .onLoad)
-pandas <- NULL
-pyvcf <- NULL
-
-.onLoad <- function(libname, pkgname) {
-  # use superassignment to update global reference to scipy
-  pandas <<- reticulate::import("pandas", delay_load = TRUE)
-  pyvcf <<- reticulate::import("pyvcf", delay_load = TRUE)
-}
+# # global reference to scipy (will be initialized in .onLoad)
+# pandas <- NULL
+# pyvcf <- NULL
+#
+# .onLoad <- function(libname, pkgname) {
+#   # use superassignment to update global reference to scipy
+#   pandas <<- reticulate::import("pandas", delay_load = TRUE)
+#   pyvcf <<- reticulate::import("pyvcf", delay_load = TRUE)
+# }
