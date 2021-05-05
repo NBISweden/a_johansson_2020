3d3ce32534c7e348fb5427668fee326d9d8fd7cb
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2021-05-05 14:20:09 +0200

    parallelize execution

--- R/count_rare.R
+++ R/count_rare.R
@@ -2,14 +2,28 @@
 #' @author Marcin Kierczak <marcin.kierczak@scilifelab.se>
 #' @description  given region coordinates, the function extracts genotypes for all loci in the region,
 #' computes minor allele frequency for each marker and returns the number of loci with maf <= threshold
-#' @param region region in chr:start-end format, e.g. 22:16191600-16193600
+#' @param regions list of regions in chr:start-end format, e.g. 22:16191600-16193600
 #' @param vcf_file path to vcf file containing genotypes
 #' @param maf_threshold maf threshold for counting loci with rare minor allele
-#' @returnthe count of loci with rare variants in the region
+#' @return a tibble with region name, count of loci and count or rare variants
 #'
-count_rare <- function(region, vcf_file, maf_threshold = 0.01) {
-  geno <- seqminer::readVCFToMatrixByRange(vcf_file, range = region)[[1]]
-  maf <- pmin(rowSums(geno)/dim(geno)[2], 1 - rowSums(geno)/dim(geno)[2])
-  rare_count <- sum(maf <= maf_threshold)
-  return(rare_count)
+count_rare <- function(regions, vcf_file, maf_threshold = 0.01) {
+  registerDoFuture()
+  y <- foreach(x = regions) %dopar% {
+    geno <- seqminer::readVCFToMatrixByRange(vcf_file, range = x)[[1]]
+    n_alleles <- 2 * dim(geno)[2]
+    maf <- pmin(rowSums(geno) / n_alleles, 1 - (rowSums(geno) / n_alleles))
+    rare_count <- sum(maf <= maf_threshold)
+    n_markers <- dim(geno)[1]
+    tmp <- list(region = x, n_loci = n_markers, n_count = rare_count)
+    tmp
+  }
+  result <- map(y, ~data.frame(.)) %>%
+    map_dfr(~mutate_all(., as.character)) %>%
+    as_tibble() %>%
+    mutate_at(c(2,3), as.integer)
+
+  return(result)
 }
+
+
