8274326c82bb8f5d84d4a13c4e1c57af76301275
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2021-05-05 14:47:10 +0200

    make count_rare compatible with the internal bed data object

--- R/count_rare.R
+++ R/count_rare.R
@@ -2,14 +2,14 @@
 #' @author Marcin Kierczak <marcin.kierczak@scilifelab.se>
 #' @description  given region coordinates, the function extracts genotypes for all loci in the region,
 #' computes minor allele frequency for each marker and returns the number of loci with maf <= threshold
-#' @param regions list of regions in chr:start-end format, e.g. 22:16191600-16193600
+#' @param regions list of regions as returned by the read_regions_bed function
 #' @param vcf_file path to vcf file containing genotypes
 #' @param maf_threshold maf threshold for counting loci with rare minor allele
 #' @return a tibble with region name, count of loci and count or rare variants
 #'
 count_rare <- function(regions, vcf_file, maf_threshold = 0.01) {
   registerDoFuture()
-  y <- foreach(x = regions) %dopar% {
+  y <- foreach(x = regions$region) %dopar% {
     geno <- seqminer::readVCFToMatrixByRange(vcf_file, range = x)[[1]]
     n_alleles <- 2 * dim(geno)[2]
     maf <- pmin(rowSums(geno) / n_alleles, 1 - (rowSums(geno) / n_alleles))
