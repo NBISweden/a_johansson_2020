7816354a9df7f6fefc2b8650420d89aa57230985
Marcin Kierczak <marcin.kierczak@scilifelab.se>
2021-05-19 15:01:22 +0200

    remove support for old file formats

--- R/get_maf.R
+++ R/get_maf.R
@@ -5,18 +5,8 @@
 #' @export
 #'
 get_maf <- function(x) {
-  obj_type <- class(x)
-  if ("gwaa.data" %in% obj_type) {
-    tmp <- summary(x@gtdata)
-    maf <- ((2 * tmp$P.22) + tmp$P.12) / (2 * tmp$NoMeasured)
-    names(maf) <- colnames(srdta@gtdata)
-  } else if ("vcfR" %in% obj_type) {
-    maf <- vcfR::maf(x)[,'Frequency']
-  } else {
-    #stop('ERROR! Data format not supported. Expecting GenABEL gwaa.data or vcfR.')
-    n_alleles <- 2 * nrow(x)
-    maf <- colSums(x) / n_alleles # allele count / total number of alleles
-  }
+  n_alleles <- 2 * nrow(x)
+  maf <- colSums(x) / n_alleles # allele count / total number of alleles
   validated <- validate_maf(maf)
   return(validated$maf)
 }
